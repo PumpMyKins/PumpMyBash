4a9dec1e26f56343132aa32bda193ad0a618ebc0
Clem-Fern <clement.coussieu@gmail.com>
2019-03-01 16:59:39 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,11 @@
-PORT=20422
-HOST="yankee.pumpmykins.eu"
-SSH_USER="backup-delta-ftb"
-SOURCE_PATH="/ftbskyblock/thermos/"
-DESTINATION_PATH="/backup-delta-ftb/"
-WEBHOOK="https://discordapp.com/api/webhooks/551028131767648257/ORfnRQprOteu90Qnn8N1xVCkTm9Hh560tG02nYWQBTM8d24-PxjmlFdrf0iTGv34Vyl-"
+#!/bin/sh
+
+PORT=
+HOST=""
+SSH_USER=""
+SOURCE_PATH=""
+DESTINATION_PATH=""
+WEBHOOK=""
 
 echo $PORT
 echo $HOST
@@ -11,24 +13,22 @@ echo $SSH_USER
 echo $SOURCE_PATH
 echo $DESTINATION_PATH
 
-bash ~/discord.sh --webhook-url=$WEBHOOK --text "Server backup started !"
+bash ./discord.sh --webhook-url=$WEBHOOK --text "Server backup started !"
 
-case "$1" in
-        --init)
-                echo "First complete backup !"
-                rsync -avHP -e "ssh -p $PORT" --numeric-ids $SSH_USER@$HOST:$SOURCE_PATH "$DESTINATION_PATH/backup-last"
-            ;;
-        --inc)
-                echo "Incremental backup !"
-                rsync -auvHP -e "ssh -p $PORT" --numeric-ids --delete --backup --backup-dir="$DESTINATION_PATH/backup-old/$DATE" $SSH_$
-                echo "Searching old directories"
-                find "$DESTINATION_PATH/backup-old/" -type d -ctime +1 -exec rm -rf {} \;
+FILE=".backup-date"   
+if [ -f $FILE ]; then
+    FILE_CONTENT=$(cat $FILE)
+    echo "File $FILE exists. DATE => $FILE_CONTENT"
+    echo "Incremental backup !"
+    rsync -auvHP -e "ssh -p $PORT" --numeric-ids --delete --backup --backup-dir="$DESTINATION_PATH/backup-old/$FILE_CONTENT" $SOURCE_PATH $SSH_USER@$HOST:"$DESTINATION_PATH/backup-last"
 
-            ;;
-        --*) echo "bad option $1"
-            ;;
-        *) echo "argument $1"
-            ;;
-esac
+else
+    echo "File $FILE does not exist."
+    echo "First complete backup !"
+    rsync -avHP -e "ssh -p $PORT" --numeric-ids $SOURCE_PATH $SSH_USER@$HOST:"$DESTINATION_PATH/backup-last"
+fi
+
+echo $DATE > $FILE
+
 echo "Discord Notifier"
-bash ~/discord.sh --webhook-url=$WEBHOOK --text "Server backup finished !"
+bash ./discord.sh --webhook-url=$WEBHOOK --text "Server backup finished !"
