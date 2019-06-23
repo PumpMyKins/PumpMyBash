c227d07bba7d83a49f37228a255418b133831c7c
Clem Fern <clemfern456@gmail.com>
2019-06-23 22:22:01 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -12,31 +12,31 @@ FILE_PREFFIX="crash-"
 CRASH_DIRECTORY="crash-report/"
 
 function ctrl_c() {
-	bash ./discord-notifier.sh --webhook-url=$WEBHOOK --text "$SERVER_NAME : Redémarrage automatique du serveur annulé depuis la console"
-	echo "** Trapped CTRL-C"
-	exit 1
+        bash ./discord-notifier.sh --webhook-url=$WEBHOOK --text "$SERVER_NAME : Redémarrage automatique du serveur annulé depuis la console"
+        echo "** Trapped CTRL-C"
+        exit 1
 }
 
-a=1
-while [ "$a" == 1 ]
+
+while :
 do
-	echo "Vous avez 6 secondes pour annuler le redémarrage avec la touche CTRL+C"
-	trap ctrl_c INT
-	sleep 6
-	
-	bash ./discord-notifier.sh --webhook-url=$WEBHOOK --text "$SERVER_NAME : Démarrage du serveur."
+        echo "Vous avez 6 secondes pour annuler le redémarrage avec la touche CTRL+C"
+        trap ctrl_c INT
+        sleep 6
 
-	exec $LAUNCH_ARG
+        bash ./discord-notifier.sh --webhook-url=$WEBHOOK --text "$SERVER_NAME : Démarrage du serveur."
 
-	DATE=`date '+%Y-%m-%d_%H.%M.*'`
-	echo "§FILE_PREFFIX$DATE"
-	if [ -f "$FILE_PREFFIX$DATE" ];
-	then
-		echo "Crash-report found !"
-		bash ./discord-notifier.sh --webhook-url=$WEBHOOK --text "$SERVER_NAME : Crash du serveur ?!? \n"
-	else
-		echo "No crash-report found."
-		bash ./discord-notifier.sh --webhook-url=$WEBHOOK --text "$SERVER_NAME : Serveur arreté."
-	fi
+        $LAUNCH_ARG
+
+        DATE=`date '+%Y-%m-%d_%H.%M.*'`
+        echo "$FILE_PREFFIX$DATE"
+        if [ -f "$CRASH_DIRECTORY/$FILE_PREFFIX$DATE" ];
+        then
+                echo "Crash-report found !"
+                bash ./discord-notifier.sh --webhook-url=$WEBHOOK --text "$SERVER_NAME : Crash du serveur ?!? \n"
+        else
+                echo "No crash-report found."
+                bash ./discord-notifier.sh --webhook-url=$WEBHOOK --text "$SERVER_NAME : Serveur arreté."
+        fi
 
 done
